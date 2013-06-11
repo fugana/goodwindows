8826adf9c918b679c3975bb3861886b573e28af8
Zumza <zumzza@yahoo.com>
2013-06-11 13:53:06 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goodwindows::Application.config.secret_token = 'e71145550c4a179d0bb986a10303fbd24389d76efe073d7bc4d87139875f68e6e57c2d5c92b856a7a7398bf000a73a22b5c2e1c752c37637a7c1015ad4c7b93c'
